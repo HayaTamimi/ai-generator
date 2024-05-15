7ebb820d1d202504489b0d1d43b1dace5365604f
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-05-15 07:50:31 +0300

    added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hi
+function generateTest (event) {
+event.preventDefault();
+
+new Typewriter("#questions", {
+  strings: "YOYOYOY",
+  autoStart: true,
+  delay: 30,
+  cursor: "",
+});
+}
+
+let inputFormElement = document.querySelector("#input-form");
+inputFormElement.addEventListener("submit", generateTest)
